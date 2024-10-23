74b97a89fca54e9c1976948fe0efacd2b2dc8fbf
Anas-raza-12 <aranasraza123@gmail.com>
2024-10-23 14:58:51 +0500

    Add Nabeel Potfolio

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+/**
+ * @copyright 2024 codewithmutahir
+ *  @license   Apache-2.0
+ */
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "./components/Theme-provider.jsx";
+import App from "./App.jsx";
+
+/**
+ * CSS links
+ */
+
+import "lenis/dist/lenis.css";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
